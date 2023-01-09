7499fb23ce19128f1c8aa71b8375ce284cc2c583
Maiwennnn <maiwennkermorgant@gmail.com>
2023-01-09 15:50:10 +0100

    Update matrice.go

--- matrice.go
+++ matrice.go
@@ -1,15 +1,25 @@
 package main
-import ftm
+
+import "fmt"
 
 func main() {
 
-	var mat [3][3]int
-	//for i:=0
-
+	var mat = *remplitMatrice(3, 1)
 	for i := 0; i < len(mat); i++ {
-		for j := 0; j < len(mat); j++ {
-			mat[i][j] = 1
-		}
+
 	}
-	
+
+}
+
+func remplitMatrice(n int, p int) *[3][3]int {
+	var mat *[3][3]int
+	for i := 0; i < len(*mat); i++ {
+		for j := 0; j < len(*mat); j++ {
+			(*mat)[i][j] = p
+			fmt.Print((*mat)[i][j], " ")
+		}
+		fmt.Println("")
+	}
+
+	return mat
 }
