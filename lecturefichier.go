16c71bc517462ff30f1a2dc6ab626e8b2401ec42
Maiwennnn <maiwennkermorgant@gmail.com>
2023-01-20 14:55:42 +0100

    Update lecturefichier.go

--- lecturefichier.go
+++ lecturefichier.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
-	"time"
 )
 
 /*func ReadFile(fileName string) string {
@@ -19,73 +18,62 @@ import (
 }*/
 
 func main() {
-	fmt.Println("J'aime Go")
-	fmt.Println("Il est ", time.Now())
+	//fmt.Println("J'aime Go")
+	//fmt.Println("Il est ", time.Now())
 	//ReadFile("test.txt")
 	/*fmt.Println("J'aime Go")
 	fmt.Println("Il est ", time.Now())*/
 	//data := ReadFile("matrice_de_test.txt \n")
 	data := "1 1 1\n1 1 1\n1 1 1"
 	print("\n", data)
-	data2 := strings.Split(data, " ")
-	fmt.Printf("\n Print de la matrice sous forme de tableau de char \n")
-	for i := 0; i < len(data2); i++ {
-		fmt.Printf(data2[i])
-	}
-
+	data2 := strings.Split(data, "\n")
 	fmt.Printf("\n Détection de la taille de la matrice \n")
-	n := 0
-	for i := 0; i < len(data2); i++ {
-		if data2[i] == "\n" {
-			n = i
-			break
-		}
-	}
+	n := len(data2)
+	fmt.Println("La matrice est de taille", n)
 
+	//On fait une matrice de strings
 	data3 := make([][]string, n)
 	for i := range data3 {
 		data3[i] = make([]string, n)
 	}
 
-	fmt.Printf("On met tous les chiffres dans un tableau\n")
-	tab := make([]string, n*n)
 	for i := range data2 {
-		splitage := strings.Split(data2[i], "\n")
+		splitage := strings.Split(data2[i], " ")
 		for j := range splitage {
-			tab = append(tab, splitage[j])
+			data3[i][j] = splitage[j]
 		}
 	}
 
-	for i := range tab {
-		fmt.Printf(tab[i] + " ")
+	fmt.Printf("\n Print de la matrice sous forme de matrice de char \n")
+	for i := 0; i < len(data3); i++ {
+		for j := 0; j < len(data3); j++ {
+			fmt.Printf(data3[i][j])
+		}
+		fmt.Println("")
 	}
 
-	fmt.Printf("\nOn convertit les strings du tableau en entiers\n")
-	tabint := make([]int, n)
-	fmt.Print("len(tab):", len(tab))
-	conversion, err := strconv.Atoi(tab[0])
-	tabint[0] = conversion
-	fmt.Print("\n tabint[0] : ", tabint[0])
-	if err != nil {
-		fmt.Println("Error during conversion")
-	}
-	/*for i := range tab {
-		fmt.Print("On entre dans la première boucle")
-		fmt.Print("i=", i)
-		conversion, err := strconv.Atoi(tab[i])
-		tabint[i] = conversion
-		fmt.Print(tabint[i])
-		if err != nil {
-			fmt.Println("Error during conversion")
-		}
-	}/*
-	for i := range tabint {
-		fmt.Print(tabint[i])
+	fmt.Println("On convertit les strings du tableau en entiers")
+
+	matrice_finale := make([][]int, n)
+	for i := range matrice_finale {
+		matrice_finale[i] = make([]int, n)
 	}
 
-	/*for i := 0; i < len(data2); i++ {
-		for j :=0; j<n; j++{
-			data3[]
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			conversion, err := strconv.Atoi(data3[i][j])
+			fmt.Println("Conversion = ", conversion)
+			matrice_finale[i][j] = conversion
+			if err != nil {
+				fmt.Println("Error during conversion")
+			}
 		}
-	}*/
+	}
+	fmt.Printf("\n Print de la matrice finale (en int) \n")
+	for i := 0; i < len(matrice_finale); i++ {
+		for j := 0; j < len(matrice_finale); j++ {
+			fmt.Print(matrice_finale[i][j], " ")
+		}
+		fmt.Println("")
+	}
 }
